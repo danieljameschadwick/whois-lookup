be26be62ba5380830650f3a00ac003f61656cbcb
Daniel Chadwick <djchadwick@outlook.com>
2023-01-27 20:33:36 +0000

    chore: whois.go (apilayer call, added api key)

--- api/v2/whois.go
+++ api/v2/whois.go
@@ -2,9 +2,29 @@ package handler
 
 import (
   "fmt"
+  "io/ioutil"
+  "log"
   "net/http"
+  "os"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+  client := &http.Client{}
+  req, err := http.NewRequest("GET", "https://api.apilayer.com/whois/check?domain=danielchadwick.co.uk", nil)
+  req.Header.Add("apikey", os.Getenv("APILAYER_API_KEY"))
+
+  res, err := client.Do(req)
+  
+  if err != nil {
+      fmt.Print(err.Error())
+      os.Exit(1)
+  }
+
+  responseData, err := ioutil.ReadAll(res.Body)
+
+  if err != nil {
+      log.Fatal(err)
+  }
+
+  fmt.Fprintf(w, string(responseData))
 }
